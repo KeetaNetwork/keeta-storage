64ebc091b765961671c4b0ba8a6b512742ef7d70
David Scheutz <dscheutz@keeta.com>
2022-09-06 10:55:05 -0700

    add basic local storage test

--- Tests/keeta-secure-storageTests/keeta_secure_storageTests.swift
+++ Tests/keeta-secure-storageTests/keeta_secure_storageTests.swift
@@ -2,4 +2,30 @@ import XCTest
 @testable import keeta_secure_storage
 
 final class keeta_secure_storageTests: XCTestCase {
+    
+    let value = "Value"
+    let key = "key"
+    
+    func basic_localStorage_test() throws {
+        let sut = LocalStorage(store: UserDefaults(suiteName: UUID().uuidString)!)
+        
+        try sut.set(value, forKey: key)
+        
+        let expectedValue: String = try sut.value(forKey: key)
+        XCTAssertEqual(expectedValue, value)
+        
+        sut.removeValue(for: key)
+        
+        let expectedNil: String? = sut.optionalValue(forKey: key)
+        XCTAssertNil(expectedNil)
+        
+        do {
+            let _: String = try sut.value(forKey: key)
+            XCTFail()
+        } catch let error as LocalStorage.Error {
+            XCTAssertEqual(error, .noDataForKey(key: key))
+        } catch {
+            XCTFail()
+        }
+    }
 }
