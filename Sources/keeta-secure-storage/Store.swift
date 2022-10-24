9ac96c3130cdf2b7bf59793728150a4bcda56c0f
David Scheutz <dscheutz@keeta.com>
2022-10-23 20:31:06 -0700

    simplify Store protocol

--- Sources/keeta-secure-storage/Store.swift
+++ Sources/keeta-secure-storage/Store.swift
@@ -2,16 +2,16 @@ import Foundation
 
 public protocol Store: AnyObject {
     func data(forKey aKey: String) -> Data?
-    func set<T>(_ value: T?, for key: String)
+    func set(_ data: Data, for key: String)
     func removeValue(for key: String)
 }
 
 extension UserDefaults: Store {
     public func removeValue(for key: String) {
-        self.removeObject(forKey: key)
+        removeObject(forKey: key)
     }
     
-    public func set<T>(_ value: T?, for key: String) {
-        self.set(value, forKey: key)
+    public func set(_ data: Data, for key: String) {
+        set(data, forKey: key)
     }
 }
